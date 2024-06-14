2c341388ef65091c25d6be67e572c13dd09af6a9
Brenno Pereira <brennopereira007@gmail.com>
2024-06-14 00:03:02 -0300

    adição de comentarios no código TCP

--- TCP/TCPClient.py
+++ TCP/TCPClient.py
@@ -1,12 +1,23 @@
-from socket import * 
+from socket import *
 
+# Define o nome do servidor (ou endereço IP) e a porta na qual o servidor está escutando
 serverName = 'hostname'
 serverPort = 12000
-clientSocket = socket(AF_INET ,SOCK_STREAM)
-clientSocket .connect ( (serverName, serverPort))
 
-sentence = input(' Input lowercase sentence : ' )
-clientSocket. send (sentence .encode ())
-modifiedSentence = clientSocket .recv (1024) 
-print('From Server: ', modifiedSentence.decode())
-clientSocket . close () 
+# Cria um socket cliente do tipo TCP (SOCK_STREAM) utilizando o protocolo de endereço IPv4 (AF_INET)
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+clientSocket.connect((serverName, serverPort))# Estabelece uma conexão com o servidor especificado pelo endereço e porta
+sentence = input('Input lowercase sentence: ')# Solicita ao usuário que insira uma frase em minúsculas
+
+
+# Envia a frase digitada pelo usuário ao servidor.'encode()' converte a string em bytes
+clientSocket.send(sentence.encode())
+
+# Recebe a resposta do servidor. Especifica que até 1024 bytes serão lidos de uma vez
+modifiedSentence = clientSocket.recv(1024)
+
+# Imprime a resposta recebida do servidor.`decode()` converte os bytes recebidos de volta para uma string
+print('From Server:', modifiedSentence.decode())
+
+clientSocket.close()# Fecha a conexão do socket com o servidor
--- TCP/TCPServer.py
+++ TCP/TCPServer.py
@@ -1,14 +1,24 @@
 from socket import *
 
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind ( ('', serverPort))
-serverSocket.listen(1)
-print('The server is ready to receive')
+serverPort = 12000 # Define a porta na qual o servidor irá escutar
 
+# Cria um socket do servidor do tipo TCP (SOCK_STREAM) utilizando o protocolo de endereço IPv4 (AF_INET)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+serverSocket.bind(('', serverPort))# Associa o socket do servidor a um endereço IP e porta específicos
+                                   # '' significa que o servidor aceita conexões em qualquer interface de rede
+
+serverSocket.listen(1)# Configura o socket do servidor para ouvir conexões de entrada
+                      # O argumento 1 especifica o número máximo de conexões pendentes
+
+print('The server is ready to receive')# Exibe uma mensagem indicando que o servidor está pronto para receber conexões
+
+# Loop infinito para manter o servidor rodando e aceitando conexões
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
+    connectionSocket, addr = serverSocket.accept() # Aceita uma nova conexão de um cliente
+    sentence = connectionSocket.recv(1024).decode() # Recebe a mensagem do cliente, até 1024 bytes 
+    capitalizedSentence = sentence.upper() # Converte a mensagem recebida para letras maiúsculas
+    connectionSocket.send(capitalizedSentence.encode()) # Envia a mensagem convertida de volta ao cliente
+ 
+    connectionSocket.close()    # Fecha a conexão com o cliente atual
+
